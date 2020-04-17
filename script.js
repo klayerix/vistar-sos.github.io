c3a4d62ad7a122d5668ff6a3a171fd7c063bbea7
klayerix <52912977+klayerix@users.noreply.github.com>
2020-04-17 18:42:25 +0300

    upload vk api

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-ymaps.ready(init);
+let logBtn = document.getElementById("signIn");
+    regBtn = document.getElementById("reg");
+    log = document.getElementById("log");
 
-function init() {
-    var geolocation = ymaps.geolocation,
-        myMap = new ymaps.Map('map', {
-            center: [55, 34],
-            zoom: 10
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
 
-    // Сравним положение, вычисленное по ip пользователя и
-    // положение, вычисленное средствами браузера.
-    geolocation.get({
-        provider: 'yandex',
-        mapStateAutoApply: true
-    }).then(function (result) {
-        // Красным цветом пометим положение, вычисленное через ip.
-        result.geoObjects.options.set('preset', 'islands#redCircleIcon');
-        result.geoObjects.get(0).properties.set({
-            balloonContentBody: 'Мое местоположение'
-        });
-        myMap.geoObjects.add(result.geoObjects);
-    });
 
-    geolocation.get({
-        provider: 'browser',
-        mapStateAutoApply: true
-    }).then(function (result) {
-        // Синим цветом пометим положение, полученное через браузер.
-        // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.
-        result.geoObjects.options.set('preset', 'islands#blueCircleIcon');
-        myMap.geoObjects.add(result.geoObjects);
-    });
-}
+logBtn.addEventListener('click', function(){
+    var logVal = log.value;
+        passVal = pass.value;
+    console.log(logVal);
+    console.log(passVal);
+})
+
+$.ajax({
+    url: "http://192.168.15.133:8081/Servlet",
+    data: ({
+        log: JSON.stringify(
+        {
+            
+        }
+        )
+    }),
+    type: "POST",
+    dataType: "json",     
+});
